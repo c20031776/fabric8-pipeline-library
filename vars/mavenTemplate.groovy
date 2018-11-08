2e5698584a970a7b9683a6067fb905728be21be2
c20031776 <c20031776@163.com>
2018-11-08 17:30:41 +0800

    Update mavenTemplate.groovy

--- vars/mavenTemplate.groovy
+++ vars/mavenTemplate.groovy
@@ -9,132 +9,87 @@ def call(Map parameters = [:], body) {
     def defaultLabel = buildId('maven')
     def label = parameters.get('label', defaultLabel)
 
-    def mavenImage = parameters.get('mavenImage', 'fabric8/maven-builder:vd81dedb')
-    def jnlpImage = (flow.isOpenShift()) ? 'fabric8/jenkins-slave-base-centos7:vb0268ae' : 'jenkinsci/jnlp-slave:2.62'
+    def mavenImage = parameters.get('mavenImage', 'openshift/jenkins-slave-maven-centos7:v4.0')
+    def jnlpImage = (flow.isOpenShift()) ? 'openshift/jenkins-slave-base-centos7:v4.0' : 'jenkinsci/jnlp-slave:2.62'
     def inheritFrom = parameters.get('inheritFrom', 'base')
+
 
     def cloud = flow.getCloudConfig()
 
-    // 0.13 introduces a breaking change when defining pod env vars so check version before creating build pod
-    if (utils.isKubernetesPluginVersion013()) {
-        echo "Kubernetes Plugin Version 013"
-        if (utils.isUseOpenShiftS2IForBuilds()) {
-            podTemplate(cloud: cloud, label: label, inheritFrom: "${inheritFrom}", serviceAccount: 'jenkins',
-                    containers: [
-                            containerTemplate(
-                                    name: 'jnlp',
-                                    image: "${jnlpImage}",
-                                    args: '${computer.jnlpmac} ${computer.name}',
-                                    workingDir: '/home/jenkins/',
-                                    resourceLimitMemory: '256Mi'),
-                            containerTemplate(
-                                    name: 'maven',
-                                    image: "${mavenImage}",
-                                    command: '/bin/sh -c',
-                                    args: 'cat',
-                                    ttyEnabled: true,
-                                    workingDir: '/home/jenkins/',
-                                    envVars: [
-                                            envVar(key: '_JAVA_OPTIONS', value: '-Duser.home=/root/ -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Dsun.zip.disableMemoryMapping=true -XX:+UseParallelGC -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -XX:GCTimeRatio=4 -XX:AdaptiveSizePolicyWeight=90 -Xms10m -Xmx192m'),
-                                            envVar(key: 'MAVEN_OPTS', value: '-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn')
-                                            ],
-                                    resourceLimitMemory: '640Mi')],
-                    volumes: [
-                            secretVolume(secretName: 'jenkins-maven-settings', mountPath: '/root/.m2'),
-                            persistentVolumeClaim(claimName: 'jenkins-mvn-local-repo', mountPath: '/root/.mvnrepository'),
-                            secretVolume(secretName: 'jenkins-release-gpg', mountPath: '/home/jenkins/.gnupg'),
-                            secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
-                            secretVolume(secretName: 'jenkins-ssh-config', mountPath: '/root/.ssh'),
-                            secretVolume(secretName: 'jenkins-git-ssh', mountPath: '/root/.ssh-git')]) {
+    /*
+        /home/jenkins is used to support the upstream maven image and this is not stored in env.JENKINS_HOME=/var/lib/jenkins
+     */
+    def javaOptions = parameters.get('javaOptions', '-Duser.home=/home/jenkins -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Dsun.zip.disableMemoryMapping=true -XX:+UseParallelGC -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -XX:GCTimeRatio=4 -XX:AdaptiveSizePolicyWeight=90 -Xms10m -Xmx192m')
 
-                body(
+    if (utils.isUseOpenShiftS2IForBuilds()) {
+        def mavenOpts = parameters.get('mavenOpts', '-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn')
 
-                )
-            }
-        } else {
-            echo "building using the docker socket"
+        podTemplate(cloud: cloud, label: label, inheritFrom: "${inheritFrom}", serviceAccount: 'jenkins',
+                containers: [
+                        containerTemplate(
+                                name: 'jnlp',
+                                image: "${jnlpImage}",
+                                args: '${computer.jnlpmac} ${computer.name}',
+                                workingDir: '/home/jenkins/',
+                                resourceLimitMemory: '256Mi'),
+                        containerTemplate(
+                                name: 'maven',
+                                image: "${mavenImage}",
+                                command: '/bin/bash -c',
+                                args: 'cat',
+                                ttyEnabled: true,
+                                workingDir: '/home/jenkins/',
+                                envVars: [
+                                        envVar(key: '_JAVA_OPTIONS', value: javaOptions),
+                                        envVar(key: 'MAVEN_OPTS', value: mavenOpts)
+                                ],
+                                resourceLimitMemory: '640Mi')],
+                volumes: [
+                        secretVolume(secretName: 'jenkins-maven-settings', mountPath: '/home/jenkins/.m2'),
+                        secretVolume(secretName: 'jenkins-release-gpg', mountPath: '/home/jenkins/.gnupg-ro'),
+                        secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
+                        secretVolume(secretName: 'jenkins-ssh-config', mountPath: '/root/.ssh-ro'),
+                        secretVolume(secretName: 'jenkins-git-ssh', mountPath: '/root/.ssh-git-ro')]) {
 
-            podTemplate(cloud: cloud,
-                    label: label,
-                    inheritFrom: "${inheritFrom}",
-                    containers: [
-                            containerTemplate (
-                                    //[name: 'jnlp', image: "${jnlpImage}", args: '${computer.jnlpmac} ${computer.name}'],
-                                    name: 'maven',
-                                    image: "${mavenImage}",
-                                    command: '/bin/sh -c',
-                                    args: 'cat',
-                                    ttyEnabled: true,
-                                    alwaysPullImage: false,
-                                    workingDir: '/home/jenkins/',
-                                    //resourceLimitMemory: '640Mi',
-                                    envVars: [
-                                            envVar(key: '_JAVA_OPTIONS', value: '-Duser.home=/root/ -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Dsun.zip.disableMemoryMapping=true -XX:+UseParallelGC -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -XX:GCTimeRatio=4 -XX:AdaptiveSizePolicyWeight=90 -Xms10m -Xmx192m'),
-                                            envVar(key: 'MAVEN_OPTS', value: '-Duser.home=/root/ -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn'),
-                                            envVar(key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/')])],
-                    volumes: [
-                            secretVolume(secretName: 'jenkins-maven-settings', mountPath: '/root/.m2'),
-                            secretVolume(secretName: 'jenkins-docker-cfg', mountPath: '/home/jenkins/.docker'),
-                            secretVolume(secretName: 'jenkins-release-gpg', mountPath: '/home/jenkins/.gnupg'),
-                            secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
-                            secretVolume(secretName: 'jenkins-ssh-config', mountPath: '/root/.ssh'),
-                            secretVolume(secretName: 'jenkins-git-ssh', mountPath: '/root/.ssh-git'),
-                            hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')])
-                    {
+            body(
 
-                        body(
-
-                        )
-                    }
+            )
         }
     } else {
-        if (utils.isUseOpenShiftS2IForBuilds()) {
-            podTemplate(cloud: cloud, label: label, inheritFrom: "${inheritFrom}", serviceAccount: 'jenkins', restartPolicy: 'OnFailure',
-                    containers: [
-                            [name: 'jnlp', image: "${jnlpImage}", args: '${computer.jnlpmac} ${computer.name}', workingDir: '/home/jenkins/',
-                             resourceLimitMemory: '512Mi'], // needs to be high to work on OSO
-                            [name: 'maven', image: "${mavenImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true, workingDir: '/home/jenkins/',
-                             envVars: [
-                                     [key: '_JAVA_OPTIONS', value: '-Duser.home=/root/ -XX:+UseParallelGC -XX:MinHeapFreeRatio=20 -XX:MaxHeapFreeRatio=40 -XX:GCTimeRatio=4 -XX:AdaptiveSizePolicyWeight=90 -Xmx256m'],
-                                     [key: 'MAVEN_OPTS', value: '-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn']
-                                     ],
-                             resourceLimitMemory: '1024Mi']],
-                    volumes: [secretVolume(secretName: 'jenkins-maven-settings', mountPath: '/root/.m2'),
-                              persistentVolumeClaim(claimName: 'jenkins-mvn-local-repo', mountPath: '/root/.mvnrepository'),
-                              secretVolume(secretName: 'jenkins-release-gpg', mountPath: '/home/jenkins/.gnupg'),
-                              secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
-                              secretVolume(secretName: 'jenkins-ssh-config', mountPath: '/root/.ssh'),
-                              secretVolume(secretName: 'jenkins-git-ssh', mountPath: '/root/.ssh-git')],
-                    envVars: [[key: 'GIT_COMMITTER_EMAIL', value: 'fabric8@googlegroups.com'], [key: 'GIT_COMMITTER_NAME', value: 'fabric8']]) {
+        echo "building using the docker socket"
+        def mavenOpts = parameters.get('mavenOpts', '-Duser.home=/home/jenkins -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn')
 
-                body(
+        podTemplate(cloud: cloud,
+                label: label,
+                inheritFrom: "${inheritFrom}",
+                containers: [
+                        containerTemplate(
+                                //[name: 'jnlp', image: "${jnlpImage}", args: '${computer.jnlpmac} ${computer.name}'],
+                                name: 'maven',
+                                image: "${mavenImage}",
+                                command: '/bin/bash -c',
+                                args: 'cat',
+                                ttyEnabled: true,
+                                alwaysPullImage: false,
+                                workingDir: '/home/jenkins/',
+                                //resourceLimitMemory: '640Mi',
+                                envVars: [
+                                        envVar(key: '_JAVA_OPTIONS', value: javaOptions),
+                                        envVar(key: 'MAVEN_OPTS', value: mavenOpts),
+                                        envVar(key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/')])],
+                volumes: [
+                        secretVolume(secretName: 'jenkins-maven-settings', mountPath: '/home/jenkins/.m2'),
+                        secretVolume(secretName: 'jenkins-docker-cfg', mountPath: '/home/jenkins/.docker'),
+                        secretVolume(secretName: 'jenkins-release-gpg', mountPath: '/home/jenkins/.gnupg-ro'),
+                        secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
+                        secretVolume(secretName: 'jenkins-ssh-config', mountPath: '/root/.ssh-ro'),
+                        secretVolume(secretName: 'jenkins-git-ssh', mountPath: '/root/.ssh-git-ro'),
+                        hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')])
+                {
 
-                )
-            }
-        } else {
-            echo "building using the docker socket"
+                    body(
 
-            podTemplate(cloud: cloud, label: label, inheritFrom: "${inheritFrom}",
-                    containers: [
-                            //[name: 'jnlp', image: "${jnlpImage}", args: '${computer.jnlpmac} ${computer.name}'],
-                            [name: 'maven', image: "${mavenImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true,
-                             envVars: [
-                                     [key: 'MAVEN_OPTS', value: '-Duser.home=/root/ -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn']]]],
-                    volumes: [secretVolume(secretName: 'jenkins-maven-settings', mountPath: '/root/.m2'),
-                              persistentVolumeClaim(claimName: 'jenkins-mvn-local-repo', mountPath: '/root/.mvnrepository'),
-                              secretVolume(secretName: 'jenkins-docker-cfg', mountPath: '/home/jenkins/.docker'),
-                              secretVolume(secretName: 'jenkins-release-gpg', mountPath: '/home/jenkins/.gnupg'),
-                              secretVolume(secretName: 'jenkins-hub-api-token', mountPath: '/home/jenkins/.apitoken'),
-                              secretVolume(secretName: 'jenkins-ssh-config', mountPath: '/root/.ssh'),
-                              secretVolume(secretName: 'jenkins-git-ssh', mountPath: '/root/.ssh-git'),
-                              hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')],
-                    envVars: [[key: 'DOCKER_HOST', value: 'unix:/var/run/docker.sock'], [key: 'DOCKER_CONFIG', value: '/home/jenkins/.docker/']]
-            ) {
-
-                body(
-
-                )
-            }
-        }
+                    )
+                }
     }
 }
